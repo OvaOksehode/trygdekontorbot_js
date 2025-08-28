ee0d231e0ecf59cf5097ec0a3ae8238b6f68bf5b
Anders Langemyr <andersla@uia.no>
2025-08-28 18:39:22 +0200

    Changed the whole ass

--- api/app.py
+++ api/app.py
@@ -3,4 +3,4 @@ from app_factory.create_app import create_app  # or just from this file if inlin
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
